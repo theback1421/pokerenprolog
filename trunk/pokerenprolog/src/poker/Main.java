ddfdbd29d5c9706282fca35f3a37a251015194ef
serujio85 <serujio85@ca203e1a-bf27-11de-8ec6-755e00613368>
2009-10-29 21:38:48 +0000

    Metodos setRank y setSuit

--- trunk/pokerenprolog/src/poker/Main.java
+++ trunk/pokerenprolog/src/poker/Main.java
@@ -17,10 +17,26 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String conexion;
+       /* String conexion;
         Conector c = new Conector();
         conexion =c.getConector();
 
+        Query q1 = new Query("rank(R)");
+
+        java.util.Hashtable[] ss4 = q1.allSolutions();
+        System.out.println( "all solutions of " + "rank(R)");
+        for ( int i=0 ; i<ss4.length ; i++ )
+        {
+            System.out.println( "X = " + ss4[i].get("R"));
+        }*/
+        Conector c = new Conector();
+        c.getConector();
+        Rank r = new Rank();
+        r.setRank();
+        System.out.println("RANKs "+r.getRanklist().toString());
+        Suit s = new Suit();
+        s.setSuit();
+        System.out.println("SUITs "+s.getSuitlist().toString());
     }
 
 
