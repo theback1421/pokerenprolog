f8e5fe429f3bd8ab06b4471d02c4418096e1c24b
serujio85 <serujio85@ca203e1a-bf27-11de-8ec6-755e00613368>
2009-11-10 11:41:13 +0000

    con el nuevo poker.pl

--- pokerenprolog/src/poker/Main.java
+++ pokerenprolog/src/poker/Main.java
@@ -31,14 +31,26 @@ public class Main {
         }*/
         Conector c = new Conector();
         c.getConector();
-        Rank actual = new Rank();
+        String t = "card(R,S)";
+        Query q = new Query(t);
+        java.util.Hashtable[] ht = q.allSolutions();
+        
+        for ( int i=0 ; i<ht.length ; i++ )
+        {
+            System.out.println( "t = " + ht[i].toString());
+        }
+
+        String t1 = "cogerCartaAleatoria(C)";
+        Query q2 = new Query(t1);
+        System.out.println("t1 = " + q2.oneSolution().get("C"));
+        /*Rank actual = new Rank();
         actual.setRank();
         actual.getRanklist().get(0);
         Baraja b = new Baraja();
         b.setBaraja();
         System.out.println("Baraja: \n");
         for(int i=0;i<b.getBarajalist().size();i++)
-        System.out.println(b.getBarajalist().get(i).getRank()+b.getBarajalist().get(i).getSuit());
+        System.out.println(b.getBarajalist().get(i).getRank()+b.getBarajalist().get(i).getSuit());*/
     }
 
 
